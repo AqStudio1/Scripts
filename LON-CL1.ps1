c05b19775350b30162c777d36ba755d584318612
AqStudio1 <zaghum.mughal.1@gmail.com>
2024-12-27 11:14:08 +0000

    Update LON-CL1.ps1

--- LON-CL1.ps1
+++ LON-CL1.ps1
@@ -1,5 +1,5 @@
-Resize-Partition -DriveLetter C -Size 75GB
-New-Partition -DiskNumber 0 -Size 25GB -AssignDriveLetter
+Resize-Partition -DriveLetter C -Size 100GB
+New-Partition -DiskNumber 0 -Size 10GB -AssignDriveLetter
 Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel "Allfiles"
 
 $zipFileUrl = "https://github.com/AqStudio1/LON-CL1/archive/refs/heads/main.zip"
@@ -8,6 +8,7 @@ Invoke-WebRequest -Uri $zipFileUrl -OutFile $zipFilePath
 Expand-Archive -Path $zipFilePath -DestinationPath ".\"
 Copy-Item -Path ".\LON-CL1-main\Files" -Destination "C:\Files" -Recurse
 Copy-Item -Path ".\LON-CL1-main\Labfiles" -Destination "E:\Labfiles" -Recurse
+Remove-Item -Path ".\repository.zip" -Recurse -Force
 Remove-Item -Path ".\LON-CL1-main" -Recurse -Force
 
 New-Item -ItemType Directory -Path "E:\Labfiles\Tools"
