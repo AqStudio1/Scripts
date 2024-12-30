cbda16ad5fa033cdc4e38257dcfa47165155f4f9
AqStudio1 <zaghum.mughal.1@gmail.com>
2024-12-30 12:58:32 +0000

    Update LON-CL1.ps1

--- LON-CL1.ps1
+++ LON-CL1.ps1
@@ -17,7 +17,3 @@ Invoke-WebRequest -Uri "https://aka.ms/WACDownload" -OutFile "E:\Labfiles\Tools\
 Get-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" | Set-NetFirewallRule -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "Network Discovery" -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Profile Domain,Private,Public -Enabled True
-
-$securePassword = ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("Adatum\Administrator", $securePassword)
-Remove-Computer -UnjoinDomainCredential $credential -PassThru -Verbose -Restart -Force
