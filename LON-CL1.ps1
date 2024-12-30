f57e35a8192536040dc698e39832e00fb42f507a
AqStudio1 <zaghum.mughal.1@gmail.com>
2024-12-30 17:10:44 +0000

    Update LON-CL1.ps1

--- LON-CL1.ps1
+++ LON-CL1.ps1
@@ -17,3 +17,5 @@ Invoke-WebRequest -Uri "https://aka.ms/WACDownload" -OutFile "E:\Labfiles\Tools\
 Get-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" | Set-NetFirewallRule -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "Network Discovery" -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Profile Domain,Private,Public -Enabled True
+
+Restart-Computer -Force
