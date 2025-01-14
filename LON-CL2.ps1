8f3e0d62b2fcf26e460514b8ed388c9380a1c371
AqStudio1 <zaghum.mughal.1@gmail.com>
2025-01-14 09:43:38 +0000

    Update LON-CL2.ps1

--- LON-CL2.ps1
+++ LON-CL2.ps1
@@ -1,7 +1,3 @@
 Get-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" | Set-NetFirewallRule -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "Network Discovery" -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Profile Domain,Private,Public -Enabled True
-
-$securePassword = ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("Adatum\Administrator", $securePassword)
-Remove-Computer -UnjoinDomainCredential $credential -PassThru -Verbose -Restart -Force
