d4f6e3745efa5d71cc875e75a6dac5e4656c8a5f
AqStudio1 <zaghum.mughal.1@gmail.com>
2025-01-14 09:46:06 +0000

    Update LON-CL2.ps1

--- LON-CL2.ps1
+++ LON-CL2.ps1
@@ -1,4 +1,5 @@
 Get-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" | Set-NetFirewallRule -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "Network Discovery" -Profile Domain,Private,Public -Enabled True
 Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Profile Domain,Private,Public -Enabled True
+
 Restart-Computer -Force
